6312b7c1b431c3ffeb4d432369a15dd21bd936f9
Brian Butterly <butterlybrian@gmail.com>
2021-04-15 07:21:48 -0500

    pyramid done

--- steps/index.js
+++ steps/index.js
@@ -17,11 +17,10 @@
 //       '### '
 //       '####'
 
-
 function steps(n) {
     for (let row = 0; row < n; row++) {
       let stair = "";
-  
+
       for (let column = 0; column < n; column++) {
         if (column <= row) {
           stair += "#";

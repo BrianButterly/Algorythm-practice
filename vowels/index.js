3ed8c745522dcd433cf395360a3ae205b09bcef5
Brian Butterly <butterlybrian@gmail.com>
2021-05-05 15:33:05 -0500

    late save

--- vowels/index.js
+++ vowels/index.js
@@ -8,20 +8,19 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  //   let count = 0;
-  //   const checker = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+  const checker = ["a", "e", "i", "o", "u"];
 
-  //   for (let char of str.toLowerCase()) {
-  //     if (checker.includes(char)) {
-  //       count++;
-  //     }
-  //   }
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
 
-  //   return count;
-
-  const matches = str.match(/[aeiou]/gi);
-  return matches ? matches.length : 0;
-  
+  return count;
 }
 
 module.exports = vowels;
+
+const matches = str.match(/[aeiou]/gi);
+return matches ? matches.length : 0;
